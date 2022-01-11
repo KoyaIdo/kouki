e042e21062bf82e42800d1435abe2f3e2d6b1488
KoyaIdo <ko80410@icloud.com>
2022-01-11 01:29:16 +0000

    01111029

---
+++ ex6-1.js
@@ -0,0 +1,2 @@
+function sayhello(){alert('Hello,Function!');}
+function sayhello61(){alert('Hello,Function!');}
---
+++ ex6-13.js
@@ -0,0 +1,2 @@
+function timechange(){document.getElementById('timechg').src='./image/logo.png';}
+function returnimg(){document.getElementById('timechg').src='./image/keiei_small.jpg';}
